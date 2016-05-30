d8b6e9328d0899a0cffbd5e6f4fea3a7976e6bd8
JRauer <johannes.rauer@hotmail.com>
2016-05-31 00:23:13 +0200

    Motor movement log
    Total number and total angle per motor added

--- main.c
+++ main.c
@@ -23,9 +23,9 @@ typedef struct motors {
 	udef_GPIO_Pin dir;
 	udef_GPIO_Pin stp;
 	udef_GPIO_Pin en;
-	//uint32_t direction;	//actual set direction
-	//uint32_t mode;		//actual set mode - For 1 turn: 1/1: 32; 1/2: 64; 1/4: 128; 1/8: 256
-	uint32_t numTotalMicroSteps;
+	uint32_t direction;	//actual set direction
+	uint32_t mode;		//actual set mode - For 1 turn: 1/1: 32; 1/2: 64; 1/4: 128; 1/8: 256
+	int32_t numTotalMicroSteps;	// in cw direction
 }motors;
 
 typedef struct move {
@@ -76,14 +76,14 @@ motors motor1 = {	{SYSCTL_PERIPH_GPIOD, OUTPUT, GPIO_PORTD_BASE, GPIO_PIN_2},
 					{SYSCTL_PERIPH_GPIOP, OUTPUT, GPIO_PORTP_BASE, GPIO_PIN_5},
 					{SYSCTL_PERIPH_GPIOM, OUTPUT, GPIO_PORTM_BASE, GPIO_PIN_7},
 					{SYSCTL_PERIPH_GPIOD, OUTPUT, GPIO_PORTD_BASE, GPIO_PIN_4},
-					0};
+					CW_TURN, FULL_STP, 0};
 
 motors motor2 = {	{SYSCTL_PERIPH_GPIOE, OUTPUT, GPIO_PORTE_BASE, GPIO_PIN_4},
 					{SYSCTL_PERIPH_GPIOC, OUTPUT, GPIO_PORTC_BASE, GPIO_PIN_7},
 					{SYSCTL_PERIPH_GPIOH, OUTPUT, GPIO_PORTH_BASE, GPIO_PIN_2},
 					{SYSCTL_PERIPH_GPIOM, OUTPUT, GPIO_PORTM_BASE, GPIO_PIN_3},
 					{SYSCTL_PERIPH_GPIOC, OUTPUT, GPIO_PORTC_BASE, GPIO_PIN_6},
-					0};
+					CW_TURN, FULL_STP, 0};
 
 /* LEDS */
 udef_GPIO_Pin leds[4] = {	{SYSCTL_PERIPH_GPION, OUTPUT, GPIO_PORTN_BASE, GPIO_PIN_1},
@@ -105,6 +105,8 @@ volatile uint32_t gui32_SysClock = 0;
 volatile move gui32_moveQ[MAX_NUM_MOVES];
 volatile uint32_t gui32_actIdx2move = 0;
 volatile uint32_t gui32_actIdx2add = 0;
+volatile float angleM1 = 0;
+volatile float angleM2 = 0;
 
 /* ----------------------- FUNCTION PROTOTYPES ----------------------- */
 void ISR_gpioUsrSW (void);								//Interrupt service Routine to Handle UsrSW Interrupt
@@ -118,18 +120,19 @@ void GPIO_Pin_write (udef_GPIO_Pin *pin2set, uint32_t h_or_l);
 void setMotorMode (motors *motor, uint32_t mode);
 void setDirection (motors *motor, uint32_t dir);
 int addMove (uint32_t mode, uint32_t direction1, uint32_t direction2, uint32_t numMicroSteps1, uint32_t numMicroSteps2, uint32_t doAgainFlag);
+void calcAngles(void);
 
 /* ----------------------- FUNCTIONS ----------------------- */
 void ISR_gpioUsrSW(void) {
     if(GPIOIntStatus(buttons[0].port_base, false) & buttons[0].pin) {	//if USRSW1 pressed
         GPIOIntClear(buttons[0].port_base, buttons[0].pin);	    	//Clear the GPIO interrupt
     	//something happens
-        addMove (FULL_STP, CW_TURN, CW_TURN, 64*1, 0, LOW);
+        addMove (FULL_STP, CW_TURN, CW_TURN, GEAR_CONV_FACTOR*32, 0, LOW);
     }
     else {
         GPIOIntClear(buttons[1].port_base, buttons[1].pin);	    	//Clear the GPIO interrupt
     	//something happens
-        addMove (FULL_STP, CW_TURN, CW_TURN, 0, 64*1, LOW);
+        addMove (HALF_STP, CCW_TURN, CCW_TURN, 0, GEAR_CONV_FACTOR*1, LOW);
     }
 }
 
@@ -182,8 +185,6 @@ void inithardware () {
     udef_GPIO_Pin_set_function(&motor2.en, 1);
 	GPIO_Pin_write(&motor1.en, LOW);
 	GPIO_Pin_write(&motor2.en, LOW);
-	GPIO_Pin_write(&motor1.dir, LOW);
-	GPIO_Pin_write(&motor2.dir, LOW);
 	setMotorMode(&motor1, FULL_STP);
 	setDirection(&motor1, CW_TURN);
 	setMotorMode(&motor2, FULL_STP);
@@ -202,7 +203,7 @@ void ms_delay(uint32_t ms){
 }
 
 void setMotorMode (motors *motor, uint32_t mode){
-	//motor->mode = mode;			//set mode in global var motor
+	motor->mode = mode;			//set mode in global var motor
 	switch(mode) {
 		case FULL_STP:
 			GPIO_Pin_write(&(motor->ms1), LOW);
@@ -224,7 +225,7 @@ void setMotorMode (motors *motor, uint32_t mode){
 }
 
 void setDirection (motors *motor, uint32_t dir){
-	//motor->direction = dir;
+	motor->direction = dir;
 	GPIO_Pin_write(&(motor->dir), dir);	//dir=LOW -> CW or HIGH -> CCW
 }
 
@@ -242,14 +243,19 @@ void initinterrupts () {
     SysTickEnable();			//Start Timer
 }
 
+void calcAngles(void){
+	angleM1 = (motor1.numTotalMicroSteps)*360 / ((MICRO_STP*GEAR_CONV_FACTOR));
+	angleM2 = (motor2.numTotalMicroSteps)*360 / ((MICRO_STP*GEAR_CONV_FACTOR));
+}
+
 int main(void) {
    inithardware();
    initinterrupts();
    IntMasterEnable();		//Enable Processor Interrupts
 
-   addMove (FULL_STP, CW_TURN, CW_TURN, 64*16, 0, LOW);
-   addMove (FULL_STP, CW_TURN, CW_TURN, 0, 64*16, LOW);
-   addMove (FULL_STP, CCW_TURN, CCW_TURN, 64*128, 64*128, LOW);
+   addMove (FULL_STP, CW_TURN, CW_TURN, GEAR_CONV_FACTOR*16, 0, LOW);
+   addMove (FULL_STP, CW_TURN, CW_TURN, 0, GEAR_CONV_FACTOR*16, LOW);
+ //  addMove (FULL_STP, CCW_TURN, CCW_TURN, GEAR_CONV_FACTOR*128, GEAR_CONV_FACTOR*128, LOW);
  //  addMove (FULL_STP, CW_TURN, CCW_TURN, 64*16, 64*32, LOW);
  //  ms_delay(16000);
 //   addMove (FULL_STP, CW_TURN, CW_TURN, 0, 64*8, LOW);
@@ -263,19 +269,18 @@ void makeStep (motors *motor){
 	}
 	else {
 		GPIO_Pin_write(&(motor->stp), HIGH);
-		motor->numTotalMicroSteps++;	//TODO: do right
+		if (motor->direction == CW_TURN) motor->numTotalMicroSteps = motor->numTotalMicroSteps + MICRO_STP/motor->mode;	//1 for MICRO_STP, 2 for HALF_STP, etc.
+		else motor->numTotalMicroSteps = motor->numTotalMicroSteps - MICRO_STP/motor->mode;
 		if(motor == &motor1) {
 			gui32_moveQ[gui32_actIdx2move].numMicroSteps1--;
 		} else {
 			gui32_moveQ[gui32_actIdx2move].numMicroSteps2--;
 		}
+		calcAngles();
 	}
 }
 
 int addMove (uint32_t mode, uint32_t direction1, uint32_t direction2, uint32_t numMicroSteps1, uint32_t numMicroSteps2, uint32_t doAgainFlag) {
-	//if(gui32_actIdx2add == MAX_NUM_MOVES) gui32_actIdx2add == 0;
-	/* CREATE MOVE */
-	//if (gui32_moveQ[gui32_actIdx2add].numMicroSteps1 == 0 && gui32_moveQ[gui32_actIdx2add].numMicroSteps2 == 0) {
 	uint8_t noFreeSpace = 1;	//set to 0 if there is a free space in the queue
 	uint8_t i = 0;
 
@@ -307,10 +312,10 @@ void ISR_SystickHandler(void) {
 	static uint8_t accFactor1 = 6;		//how slow should acceloration start (higher = slower)
 	static uint8_t accFactor2 = 6;		//how slow should acceloration start (higher = slower)
 	static uint8_t accSpeed = 100;		//how fast should it become faster (lower = faster)
-	static uint8_t sameDirFlag1 = 0;	//set if last motor direction == new motor direction -> no acceloration necessary
+	/*static uint8_t sameDirFlag1 = 0;	//set if last motor direction == new motor direction -> no acceloration necessary
 	static uint8_t sameDirFlag2 = 0;
 	static uint8_t systickcounterTotalwhenFinished1 = 0;	//to safe number of systicks when motor stopped moving
-	static uint8_t systickcounterTotalwhenFinished2 = 0;
+	static uint8_t systickcounterTotalwhenFinished2 = 0;*/
 
 	systickcounter++;
 	systickcounterTotal++;
@@ -334,7 +339,7 @@ void ISR_SystickHandler(void) {
 		accFactor2 = 6;
 		systickcounterTotal = 0;
 
-		if(gui32_actIdx2move == MAX_NUM_MOVES-1) gui32_actIdx2move = 0;
+		if(gui32_actIdx2move == MAX_NUM_MOVES-1) gui32_actIdx2move = 0;		//if end of queue -> start over again
 		else gui32_actIdx2move++;
 		setDirection(&motor1, gui32_moveQ[gui32_actIdx2move].direction1);
 		setMotorMode(&motor1, gui32_moveQ[gui32_actIdx2move].mode);
@@ -343,3 +348,4 @@ void ISR_SystickHandler(void) {
 	}
 }
 
+/* TODO: Funktion die gerufen wird und mode/direction etc. setzt wenn neuer move abgearbeitet wird*/
